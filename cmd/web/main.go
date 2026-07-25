22b3120ad4421b4e1d2064f7029f1a71c9214ac4
SyDan0day <1293047033@qq.com>
2026-07-25 22:28:16 +0800

    chapter 1 done

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,13 +5,11 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome!"))
-}
-
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on port:4000")
 	err := http.ListenAndServe(":4000", mux)
