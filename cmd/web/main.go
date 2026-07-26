034855abdb4e15da483c24960ebfc3ceb1d720ae
SyDan0day <1293047033@qq.com>
2026-07-26 15:54:00 +0800

    chapter 2 really done.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,19 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	// 实际上
+	// fileServer := http.FileServer(http.Dir("./ui"))
+	// mux.Handle("/static/", fileServer)也可以，但是路由注册得太宽，不太好
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	//其实写成mux.Handle("/", http.HandlerFunc(home))也一样
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on port:4000")
+	//ListenAndServe的第二个参数是Handler,mux也实现了ServeHTTP方法，因此也可以视作特殊的http.Handler
 	err := http.ListenAndServe(":4000", mux)
 	if err != nil {
 		log.Fatal(err)
