469323a6298fe657deb8f0ef0b741406d164687a
SyDan0day <1293047033@qq.com>
2026-07-26 19:46:40 +0800

    dependency injection done

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -18,18 +17,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 		"./ui/html/pages/home.tmpl"}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -38,7 +37,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific view on id:%d\n", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("allow", http.MethodPost)
 		//w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,22 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
 	// 实际上
 	// fileServer := http.FileServer(http.Dir("./ui"))
@@ -21,14 +31,19 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	//其实写成mux.Handle("/", http.HandlerFunc(home))也一样
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	infoLog.Printf("Starting server on port%s", *addr)
 	//ListenAndServe的第二个参数是Handler,mux也实现了ServeHTTP方法，因此也可以视作特殊的http.Handler
-	err := http.ListenAndServe(*addr, mux)
-	if err != nil {
-		errorLog.Fatal(err)
+	//http.ListenAndServe实际上是使用默认参数创建了一个http.Server，并调用它的ListenAndServe方法。
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
 	}
+	infoLog.Printf("Starting server on port%s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
